67cb1762c9182c1b467d1d203cf935c50a98dd75
Sibongiseni Tembe <stembe@c3r2s1.wethinkcode.co.za>
2020-06-21 16:49:03 +0200

    update

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -5,12 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: stembe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/21 12:57:28 by stembe            #+#    #+#             */
-/*   Updated: 2020/06/21 13:05:39 by stembe           ###   ########.fr       */
+/*   Created: 2020/06/21 14:49:01 by stembe            #+#    #+#             */
+/*   Updated: 2020/06/21 16:46:18 by stembe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int ft_iterative_factorial(int nb){
-	
-	return 0;
+	int i;
+	int result;
+
+	count = 1;
+	result = 1;
+
+	if (nb < 0 || nb > 12){
+		return 0;
+	}
+	while (i <= nb)
+	{
+		result *= count;
+		++count;
+	}
+	return (result);
 }
